aa32d188f1163e51e8a762adf969a50b7a7fb551
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-02-13 14:04:16 -0500

    input validation for login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     BrowserModule,
+		FormsModule,
 		AppRoutingModule,
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,4 +12,8 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+	onSubmit(f: any) {
+		console.log(f.value);
+	}
+
 }
